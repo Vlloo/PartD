02162d2746d6cfbf2def704d6dbb34cb26127c8a
vlloo <vlobro@outlook.com>
2021-01-14 18:13:37 +1300

    Extra commit

--- PartD/src/Main.java
+++ PartD/src/Main.java
@@ -6,6 +6,7 @@ public class Main {
 		System.out.println("My favorite color is red");
 		System.out.println("My favorite mode of transport is car");
 		System.out.println("The best day of the week is Monday");
+		System.out.println("Extra commit");
 	}
 	
 	 public static void main(String[] args) {
