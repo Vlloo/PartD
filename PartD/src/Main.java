36e36c96a15d0cd629bf6f336de0b68a687f71fa
vlloo <vlobro@outlook.com>
2021-01-14 18:11:52 +1300

    The best day of the week is Monday

--- PartD/src/Main.java
+++ PartD/src/Main.java
@@ -5,6 +5,7 @@ public class Main {
 		System.out.println("Hi, my name is Sander");
 		System.out.println("My favorite color is red");
 		System.out.println("My favorite mode of transport is car");
+		System.out.println("The best day of the week is Monday");
 	}
 	
 	 public static void main(String[] args) {
