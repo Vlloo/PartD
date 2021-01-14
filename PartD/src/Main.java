03515f432bf8c52f72815a29696556da5c758c63
vlloo <vlobro@outlook.com>
2021-01-14 18:01:48 +1300

    My favorite color is red

--- PartD/src/Main.java
+++ PartD/src/Main.java
@@ -3,6 +3,7 @@ public class Main {
 	
 	public Main() {
 		System.out.println("Hi, my name is Sander");
+		System.out.println("My favorite color is red");
 	}
 	
 	 public static void main(String[] args) {
