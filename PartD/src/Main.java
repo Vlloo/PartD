0dd27648e5c94e3d9ce28c468972510a621f7198
vlloo <vlobro@outlook.com>
2021-01-14 18:10:17 +1300

    My favorite mode of transport is car

--- PartD/src/Main.java
+++ PartD/src/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	public Main() {
 		System.out.println("Hi, my name is Sander");
 		System.out.println("My favorite color is red");
+		System.out.println("My favorite mode of transport is car");
 	}
 	
 	 public static void main(String[] args) {
